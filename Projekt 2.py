0d11febe6ccaf09af6d0200b3f58067722235b64
Georgios  Manioudakis <giorgos.manioudakis@gmail.com>
2023-05-04 12:41:20 +0300

    Projekt 2

---
+++ Projekt 2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+#Uppgift 1
+
+
+
+
+
+def v(t):
+    return np.array([[x_t - w(t)], [y_t]])
+
+
+def Fd(a, t):
+    return -a * np.abs(v(t)) * v(t)
+
+
+def g(y):
+    return 3.986 * 10**14 / (6.371 * 10**6 + y)**2
+
+
+def c(c0, y):
+    return c0 * np.exp(-1 * 10**(-4) * y)
+
+
+def x_tt(y, t):
+    return -c(y) * np.abs(v(t)) * (x_t - w(t))
+
+
+def y_tt(y, t):
+    return -c(y) * np.abs(v(t)) * y_t - g(y)
+
+
+
+
+
+x0 = 0
+y0 = 0
